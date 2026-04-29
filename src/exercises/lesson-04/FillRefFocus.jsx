82b1d687b521e2d79c0aa380c2d420331c7c1287
mishasro72 <mishasro72@gmail.com>
2026-04-28 20:25:45 -0400

    Add lesson 4 assignment

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,14 +1,19 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+import { useRef } from 'react';
+
 export default function FillRefFocus() {
-  function focusInput() {}
+  const formRef = useRef(null);
+
+  function focusInput() {
+    formRef.current.focus();
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
 
-      <input type="text" placeholder="Type here..." />
-
+      <input ref={formRef} type="text" placeholder="Type here..." />
       <button onClick={focusInput}>Focus Input</button>
     </div>
   );
--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,16 +1,29 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
+import { useRef } from 'react';
+
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  const clickCount = useRef(0); // ← incorrect implementation
+  const buttonRef = useRef(null);
 
   function handleClick() {
-    clickCount++;
+    clickCount.current++;
+
+    if (buttonRef.current) {
+      if (clickCount.current === 1) {
+        buttonRef.current.textContent = '1 Click';
+      } else {
+        buttonRef.current.textContent = `${clickCount.current} Clicks`;
+      }
+    }
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+      <button ref={buttonRef} onClick={handleClick}>
+        Click Me
+      </button>
     </div>
   );
 }
